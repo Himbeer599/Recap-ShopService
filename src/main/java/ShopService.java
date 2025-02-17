b5bcaf37d38735cdf7a65db93742976d7efb910c
himbeer599 <15355204+himbeer599@user.noreply.gitee.com>
2025-02-17 14:37:54 +0100

    add method getOrderByStatus

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
-    private OrderRepo orderRepo = new OrderMapRepo();
+    //    private OrderRepo orderRepo = new OrderMapRepo();
+    private OrderRepo orders = new OrderListRepo();
 
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
@@ -17,8 +19,18 @@ public class ShopService {
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+//        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+//        return orderRepo.addOrder(newOrder);
 
-        return orderRepo.addOrder(newOrder);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
+        return orders.addOrder(newOrder);
+
+    }
+
+    // New method to filter orders by order status using streams
+    public List<Order> getOrderByStatus(OrderStatus orderStatus){
+        return orders.getOrders().stream()
+                .filter(order->order.orderStatus().equals(orderStatus))
+                .collect(Collectors.toList());
     }
 }
